60ced0530606e2fcba351082d78c9d390518e8bf
Matthew Teferi <metthewsteferi@gmail.com>
2026-08-27 03:19:10 -0700

    fix: add raw sql to create missing columns on Render

--- backend/build.sh
+++ backend/build.sh
@@ -10,5 +10,22 @@ pip install -r requirements.txt
 # If GDAL is an issue on Render, user might need to use a Docker deployment.
 # We will use collectstatic and migrate.
 python manage.py collectstatic --no-input
+
+# Run raw SQL to add new columns because makemigrations dynamically skips on Render
+python -c "
+import os, django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+from django.db import connection
+try:
+    with connection.cursor() as cursor:
+        cursor.execute('ALTER TABLE core_report ADD COLUMN aanaa VARCHAR(255) NULL;')
+        cursor.execute('ALTER TABLE core_report ADD COLUMN kuta_magaalaa VARCHAR(255) NULL;')
+        cursor.execute('ALTER TABLE core_report ADD COLUMN iddoo_addaa VARCHAR(255) NULL;')
+    print('Successfully added missing address columns to core_report.')
+except Exception as e:
+    print('Columns might already exist:', e)
+"
+
 python manage.py makemigrations
 python manage.py migrate
