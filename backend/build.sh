1cfdff8166b6a3afd8e84e561af6c9d6c4578205
Matthew Teferi <metthewsteferi@gmail.com>
2026-08-27 02:34:35 -0700

    chore: add makemigrations to build script

--- backend/build.sh
+++ backend/build.sh
@@ -10,4 +10,5 @@ pip install -r requirements.txt
 # If GDAL is an issue on Render, user might need to use a Docker deployment.
 # We will use collectstatic and migrate.
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
